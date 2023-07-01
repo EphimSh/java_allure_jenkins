f91634437717cafb5c5fcae0ab1dc004c1abc993
EphimSh <ephimizm@gmail.com>
2023-07-01 22:16:35 +0300

    update TestBase.java

--- src/test/java/guru/qa/homework/tests/TestBase.java
+++ src/test/java/guru/qa/homework/tests/TestBase.java
@@ -1,7 +1,10 @@
 package guru.qa.homework.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -12,6 +15,7 @@ import static com.google.common.collect.ImmutableMap.of;
 public class TestBase {
     @BeforeAll
     static void init(){
+
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/";
@@ -23,5 +27,10 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
